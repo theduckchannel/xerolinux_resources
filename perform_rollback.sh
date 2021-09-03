7c9a99d2c827d7e2324247c61471eeedd9095bab
theduckchannel <fredcox@gmail.com>
2021-09-02 22:49:24 -0300

    ok

--- perform_rollback.sh
+++ perform_rollback.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
+# Install rollback-git with paru -S rollback-git
 
-sudo mount -t btrfs -o subvolid=5 /dev/sda2 /mnt
+# edit /etc/rollback.conf and change the line 12 to:
+# subvolid5 = /mnt
 
-# First you search what snapshot ID you want rollback with the following command:
+# mount the btrfs root subvol=@ with:
+
+# sudo mount -t btrfs -o subvolid=5 /dev/sda2 /mnt
+
+# First choose what snapshot ID you want rollback with the following command:
 # sudo snapper -c root list
 
 # And if you want rolback to snapshot id = 5, type command bellow
 
 sudo rollback 5
+
+# Reboot and its ok, you rollback. 
